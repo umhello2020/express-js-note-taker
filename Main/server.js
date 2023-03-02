98145b416ce13b49d53902735a3c306afaac4cce
Olivia Gilbert <umhello2020@gmail.com>
2023-03-01 20:06:03 -0600

    finished server.js and added connected the file to store the notes to the api and html route files

--- Main/server.js
+++ Main/server.js
@@ -1,6 +1,17 @@
 const express = require('express');
-const fs = require('fs');
-const path = require('path');
-const util = require('util');
+const apiRoute = require('./routes/apiRoutes');
+const htmlRoute = require('./routes/htmlRoutes');
 
-const uuid = require('')
+const app = express();
+const PORT = 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extedned: true }));
+app.use(express.static('public'));
+
+app.use('/api', apiRoute);
+app.use('/', htmlRoute);
+
+app.listen(PORT, () =>
+    console.log(`App listening at http://localhost:${PORT}`)
+);
