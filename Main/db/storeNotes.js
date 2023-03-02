36b2565da62104a92ec8395668dbcf04d53d52a7
Olivia Gilbert <umhello2020@gmail.com>
2023-03-01 20:50:16 -0600

    finsihed htmlRoute file, started apiRoute file, created and added base to storeNotes file

--- Main/db/storeNotes.js
+++ Main/db/storeNotes.js
@@ -1,1 +1,5 @@
+const fs = require('fs');
+const utils = require('utils');
+const uuid = require('uuid');
+
 module.exports = storeNotes;
--- Main/routes/htmlRoutes.js
+++ Main/routes/htmlRoutes.js
@@ -1,1 +1,12 @@
-const storeNotes = require('../db/storeNotes');
+const path = require('path');
+const router = require('express').Router();
+
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/notes.html'))
+});
+
+router.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'))
+});
+
+module.exports = router;
